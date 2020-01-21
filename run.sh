3c19471b09a2a50358f518909ce450d3633dc008
Gustavo Van Erven <gvanerven@gmail.com>
2020-01-21 14:37:48 -0300

    change in run.sh, venv commented

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #python3.6 -m venv venv
 #source venv/bin/activate
-pip install -e .
+#pip install -e .
 python examples/automl/toy.py data=abalone3.arff storage=sqlite db=/tmp/paje iter=10 seed=123
 #deactivate 
